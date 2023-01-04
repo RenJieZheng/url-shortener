a307664b16f1850a0686ef62c37fc6d85b34dc9e
RenJieZheng <42659888+RenJieZheng@users.noreply.github.com>
2023-01-04 14:17:16 -0500

    added body-parser to parse post requests

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,21 @@
-const express = require("express");
-const router = express.Router();
+const router = require("express").Router();
+const path = require("path");
+
+const homepage = path.join(__dirname, "..", "views", "index.html");
 
 router.get("/", (req, res) => {
-    console.log("/ endpoint");
-    res.send("/ endpoint")
+    res.sendFile(homepage);
 });
+
+router.get("/shorturl", (req, res) => {
+    console.log(req.query);
+    res.send("/shorturl get endpoint");
+});
+
+router.post("/shorturl", (req, res) => {
+    console.log(req.body);
+    res.send("/shorturl post endpoint");
+});
+
 
 module.exports = router;
